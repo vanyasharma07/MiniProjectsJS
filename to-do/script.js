4bed5f23d8a8bda7cff6653ca1d5c8c27defd797
Vanya Sharma <vanya2007sharma@gmail.com>
2025-07-02 15:30:32 +0530

    load tasks from localStorage on page load

--- to-do/script.js
+++ to-do/script.js
@@ -1,8 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
 const todoInput = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
-let tasks =[];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 addTaskButton.addEventListener('click', () => {
     const taskText = todoInput.value.trim()
@@ -23,3 +24,4 @@ addTaskButton.addEventListener('click', () => {
 function saveTasks(){
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+});
