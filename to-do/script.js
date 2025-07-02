984301e8859166eadb8c34c62cfd8ac311fd6064
Vanya Sharma <vanya2007sharma@gmail.com>
2025-07-02 19:09:16 +0530

    feat: create renderTask function to display tasks with completion status and delete button

--- to-do/script.js
+++ to-do/script.js
@@ -5,6 +5,8 @@ const todoList = document.getElementById("todo-list");
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+tasks.forEach((task) => renderTask(task));
+
 addTaskButton.addEventListener('click', () => {
     const taskText = todoInput.value.trim()
     if(taskText === "") return;
@@ -20,6 +22,18 @@ addTaskButton.addEventListener('click', () => {
     console.log(tasks);
 });
 //now we have created an array where we can add tasks
+
+function renderTask(task){
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed){
+        li.classList.add("completed");
+    };
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>`;
+    todoList.appendChild(li);
+}
 
 function saveTasks(){
     localStorage.setItem('tasks', JSON.stringify(tasks));
