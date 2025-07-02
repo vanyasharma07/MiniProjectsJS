1317968dfc70f044f5893651f3fc30bf4582fe44
Vanya Sharma <vanya2007sharma@gmail.com>
2025-07-02 13:55:18 +0530

    added event listener to the task-button

--- to-do/script.js
+++ to-do/script.js
@@ -2,3 +2,11 @@ const todoInput = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
+let tasks =[];
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = todoInput.ariaValueMax.trim()
+    if(taskText === "") return;
+
+
+})
