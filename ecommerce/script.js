253206313aab6a94ed8e8ef1e8c2d81270d151fa
Vanya Sharma <vanya2007sharma@gmail.com>
2025-07-12 16:38:49 +0530

    added products and grabbed DOM elements

---
+++ ecommerce/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const products = [
+        {
+            id:1,
+            name:"Product 1",
+            price:29.99
+        },
+        {
+            id:2,
+            name:"Product 2",
+            price:49.99
+        },
+        {
+            id:3,
+            name:"Product 3",
+            price:19.99
+        },
+    ];
+
+    const cart = [];
+
+    const productList = document.getElementById("product-list");
+    const cartItems = document.getElementById("cart-items");
+    const emptyCartMessage = document.getElementById("empty-cart");
+    const cartTotalMessage = document.getElementById("cart-total");
+    const totalPriceDisplay = document.getElementById("total-price");
+    const checkoutBtn = document.getElementById("checkout-btn");
+
+    products.forEach( product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+        productDiv.innerHTML =`
+        <span>${product.name} - $${product.price}</span>
+        <button data-id="${product.id}">Add to cart</button>
+        `;
+
+    });
+});
